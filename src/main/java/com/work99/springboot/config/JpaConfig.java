2bac60098a42ce48720d6e624fe7004012832295
Yang JoungBeom <very2sky@naver.com>
2022-03-24 16:26:38 +0900

    google+naver 연동/ argumentResolver 세션로그인/spring security

---
+++ src/main/java/com/work99/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.work99.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
